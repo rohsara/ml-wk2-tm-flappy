ce11d026f31cbbdf4297bc7470a045d77fe42d52
sararo <ro.hsara@gmail.com>
2021-09-16 22:53:14 -0400

    flappy bird ver 1

--- sketch.js
+++ sketch.js
@@ -1,11 +1,39 @@
 let bird;
+let pipes = [];
 
 function setup() {
 	createCanvas(400, 600);
 	bird = new Bird();
+	pipes.push(new Pipe());
 }
 
 function draw() {
 	background(0);
+
+	for(let i = pipes.length-1; i >= 0; i--){
+		pipes[i].show();
+		pipes[i].update();
+
+		if(pipes[i].hits(bird)){
+			console.log("HIT");
+		}
+		
+		if(pipes[i].offscreen()){
+			pipes.splice(i, 1);
+		}
+	}
+
+	bird.update();
 	bird.show();
+
+	if(frameCount % 100 === 0){
+		pipes.push(new Pipe()); 		
+	}
+}
+
+function keyPressed(){
+	if(key === ' '){
+		// console.log("Spacebar")
+		bird.up();
+	}
 }
